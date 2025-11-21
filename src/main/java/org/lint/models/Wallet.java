40d6d9e948aa509d4e253aec2735f2d134380505
TsioryR98 <hei.tsiory.2@gmail.com>
2025-11-21 13:21:06 +0300

    fix : fixing issues detected by sonarqube by using logger

--- src/main/java/org/lint/models/Wallet.java
+++ src/main/java/org/lint/models/Wallet.java
@@ -11,4 +11,38 @@ public class Wallet {
     private double amount;
     private Status status;
 
+    public void checkWalletOpen() {
+        if(this.status != Status.OPEN){
+            throw new IllegalStateException("Wallet is closed");
+        }
+    }
+    public void open() {
+        this.status = Status.OPEN;
+    }
+
+    public void close() {
+        this.status = Status.CLOSED;
+    }
+
+    public double addVola(double vola) {
+        checkWalletOpen();
+        if(vola < 0){
+            throw new IllegalArgumentException("Cannot add negative amount");
+        }
+        this.amount += vola;
+        return this.amount;
+    }
+    public double getVola(double vola) {
+        if (this.amount >= vola) {
+            this.amount -= vola;
+        } else {
+            vola = this.amount;
+            this.amount = 0;
+        }
+        return vola;
+    }
+    public double checkVola() {
+        checkWalletOpen();
+        return this.amount;
+    }
 }
